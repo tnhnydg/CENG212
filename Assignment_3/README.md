daa5fe6b41476d4358e2d5f68e147698cde70f80
Tunahan <44303456+tnhnydg@users.noreply.github.com>
2019-03-03 12:16:15 +0300

    Add files via upload

---
+++ Assignment_3/README.md
@@ -0,0 +1,3 @@
+# CENG212 Assignment_3
+
+![alt text](https://github.com/tnhnydg/CENG212/blob/master/Assignment_3/Materials/assignment_3.png)
---
+++ Assignment_3/g39.pl
@@ -0,0 +1,277 @@
+% Tunahan Yadigarbigun-Furkan Emre Yilmaz 230201003-230201057
+
+
+iris("1",  5.1, 3.5, 1.4, 0.2, "setosa").
+iris("2",  4.9, 3,   1.4, 0.2, "setosa").
+iris("3",  4.7, 3.2, 1.3, 0.2, "setosa").
+iris("4",  4.6, 3.1, 1.5, 0.2, "setosa").
+iris("5",  5,   3.6, 1.4, 0.2, "setosa").
+iris("6",  5.4, 3.9, 1.7, 0.4, "setosa").
+iris("7",  4.6, 3.4, 1.4, 0.3, "setosa").
+iris("8",  5,   3.4, 1.5, 0.2, "setosa").
+iris("9",  4.4, 2.9, 1.4, 0.2, "setosa").
+iris("10", 4.9, 3.1, 1.5, 0.1, "setosa").
+iris("11", 5.4, 3.7, 1.5, 0.2, "setosa").
+iris("12", 4.8, 3.4, 1.6, 0.2, "setosa").
+iris("13", 4.8, 3,   1.4, 0.1, "setosa").
+iris("14", 4.3, 3,   1.1, 0.1, "setosa").
+iris("15", 5.8, 4,   1.2, 0.2, "setosa").
+iris("16", 5.7, 4.4, 1.5, 0.4, "setosa").
+iris("17", 5.4, 3.9, 1.3, 0.4, "setosa").
+iris("18", 5.1, 3.5, 1.4, 0.3, "setosa").
+iris("19", 5.7, 3.8, 1.7, 0.3, "setosa").
+iris("20", 5.1, 3.8, 1.5, 0.3, "setosa").
+iris("21", 5.4, 3.4, 1.7, 0.2, "setosa").
+iris("22", 5.1, 3.7, 1.5, 0.4, "setosa").
+iris("23", 4.6, 3.6, 1,   0.2, "setosa").
+iris("24", 5.1, 3.3, 1.7, 0.5, "setosa").
+iris("25", 4.8, 3.4, 1.9, 0.2, "setosa").
+iris("26", 5 3, 1.6, 0.2, 0.0, "setosa").
+iris("27", 5,   3.4, 1.6, 0.4, "setosa").
+iris("28", 5.2, 3.5, 1.5, 0.2, "setosa").
+iris("29", 5.2, 3.4, 1.4, 0.2, "setosa").
+iris("30", 4.7, 3.2, 1.6, 0.2, "setosa").
+iris("31", 4.8, 3.1, 1.6, 0.2, "setosa").
+iris("32", 5.4, 3.4, 1.5, 0.4, "setosa").
+iris("33", 5.2, 4.1, 1.5, 0.1, "setosa").
+iris("34", 5.5, 4.2, 1.4, 0.2, "setosa").
+iris("35", 4.9, 3.1, 1.5, 0.2, "setosa").
+iris("36", 5,   3.2, 1.2, 0.2, "setosa").
+iris("37", 5.5, 3.5, 1.3, 0.2, "setosa").
+iris("38", 4.9, 3.6, 1.4, 0.1, "setosa").
+iris("39", 4.4, 3,  1.3, 0.2, "setosa").
+iris("40", 5.1, 3.4, 1.5, 0.2, "setosa").
+iris("41", 5,   3.5, 1.3, 0.3, "setosa").
+iris("42", 4.5, 2.3, 1.3, 0.3, "setosa").
+iris("43", 4.4, 3.2, 1.3, 0.2, "setosa").
+iris("44", 5,   3.5, 1.6, 0.6, "setosa").
+iris("45", 5.1, 3.8, 1.9, 0.4, "setosa").
+iris("46", 4.8, 3,   1.4, 0.3, "setosa").
+iris("47", 5.1, 3.8, 1.6, 0.2, "setosa").
+iris("48", 4.6, 3.2, 1.4, 0.2, "setosa").
+iris("49", 5.3, 3.7, 1.5, 0.2, "setosa").
+iris("50", 5,   3.3, 1.4, 0.2, "setosa").
+iris("51", 7,   3.2, 4.7, 1.4, "versicolor").
+iris("52", 6.4, 3.2, 4.5, 1.5, "versicolor").
+iris("53", 6.9, 3.1, 4.9, 1.5, "versicolor").
+iris("54", 5.5, 2.3, 4,   1.3, "versicolor").
+iris("55", 6.5, 2.8, 4.6, 1.5, "versicolor").
+iris("56", 5.7, 2.8, 4.5, 1.3, "versicolor").
+iris("57", 6.3, 3.3, 4.7, 1.6, "versicolor").
+iris("58", 4.9, 2.4, 3.3, 1, "versicolor").
+iris("59", 6.6, 2.9, 4.6, 1.3, "versicolor").
+iris("60", 5.2, 2.7, 3.9, 1.4, "versicolor").
+iris("61", 5,   2,   3.5, 1, "versicolor").
+iris("62", 5.9, 3,   4.2, 1.5, "versicolor").
+iris("63", 6,   2.2, 4,   1, "versicolor").
+iris("64", 6.1, 2.9, 4.7, 1.4, "versicolor").
+iris("65", 5.6, 2.9, 3.6, 1.3, "versicolor").
+iris("66", 6.7, 3.1, 4.4, 1.4, "versicolor").
+iris("67", 5.6, 3,   4.5, 1.5, "versicolor").
+iris("68", 5.8, 2.7, 4.1, 1, "versicolor").
+iris("69", 6.2, 2.2, 4.5, 1.5, "versicolor").
+iris("70", 5.6, 2.5, 3.9, 1.1, "versicolor").
+iris("71", 5.9, 3.2, 4.8, 1.8, "versicolor").
+iris("72", 6.1, 2.8, 4, 1.3, "versicolor").
+iris("73", 6.3, 2.5, 4.9, 1.5, "versicolor").
+iris("74", 6.1, 2.8, 4.7, 1.2, "versicolor").
+iris("75", 6.4, 2.9, 4.3, 1.3, "versicolor").
+iris("76", 6.6, 3,   4.4, 1.4, "versicolor").
+iris("77", 6.8, 2.8, 4.8, 1.4, "versicolor").
+iris("78", 6.7, 3, 5, 1.7, "versicolor").
+iris("79", 6,   2.9, 4.5, 1.5, "versicolor").
+iris("80", 5.7, 2.6, 3.5, 1, "versicolor").
+iris("81", 5.5, 2.4, 3.8, 1.1, "versicolor").
+iris("82", 5.5, 2.4, 3.7, 1, "versicolor").
+iris("83", 5.8, 2.7, 3.9, 1.2, "versicolor").
+iris("84", 6,   2.7, 5.1, 1.6, "versicolor").
+iris("85", 5.4, 3,   4.5, 1.5, "versicolor").
+iris("86", 6,   3.4, 4.5, 1.6, "versicolor").
+iris("87", 6.7,  3.1, 4.7, 1.5, "versicolor").
+iris("88", 6.3,  2.3, 4.4, 1.3, "versicolor").
+iris("89", 5.6,  3,   4.1, 1.3, "versicolor").
+iris("90", 5.5,  2.5, 4,   1.3, "versicolor").
+iris("91", 5.5,  2.6, 4.4, 1.2, "versicolor").
+iris("92", 6.1,  3,   4.6, 1.4, "versicolor").
+iris("93", 5.8,  2.6, 4,   1.2, "versicolor").
+iris("94", 5,    2.3, 3.3, 1, "versicolor").
+iris("95", 5.6,  2.7, 4.2, 1.3, "versicolor").
+iris("96", 5.7,  3,   4.2, 1.2, "versicolor").
+iris("97", 5.7,  2.9, 4.2, 1.3, "versicolor").
+iris("98", 6.2,  2.9, 4.3, 1.3, "versicolor").
+iris("99", 5.1,  2.5, 3,   1.1, "versicolor").
+iris("100",5.7,  2.8, 4.1, 1.3, "versicolor").
+iris("101",6.3,  3.3, 6, 2.5, "virginica").
+iris("102",5.8,  2.7, 5.1, 1.9, "virginica").
+iris("103",7.1,  3,   5.9, 2.1, "virginica").
+iris("104",6.3,  2.9, 5.6, 1.8, "virginica").
+iris("105", 6.5, 3, 5.8, 2.2, "virginica").
+iris("106", 7.6, 3, 6.6, 2.1, "virginica").
+iris("107", 4.9, 2.5, 4.5, 1.7, "virginica").
+iris("108", 7.3, 2.9, 6.3, 1.8, "virginica").
+iris("109", 6.7, 2.5, 5.8, 1.8, "virginica").
+iris("110", 7.2, 3.6, 6.1, 2.5, "virginica").
+iris("111", 6.5, 3.2, 5.1, 2, "virginica").
+iris("112", 6.4, 2.7, 5.3, 1.9, "virginica").
+iris("113", 6.8, 3, 5.5, 2.1, "virginica").
+iris("114", 5.7, 2.5, 5, 2, "virginica").
+iris("115", 5.8, 2.8, 5.1, 2.4, "virginica").
+iris("116", 6.4, 3.2, 5.3, 2.3, "virginica").
+iris("117", 6.5, 3, 5.5, 1.8, "virginica").
+iris("118", 7.7, 3.8, 6.7, 2.2, "virginica").
+iris("119", 7.7, 2.6, 6.9, 2.3, "virginica").
+iris("120", 6, 2.2, 5, 1.5, "virginica").
+iris("121", 6.9, 3.2, 5.7, 2.3, "virginica").
+iris("122", 5.6, 2.8, 4.9, 2, "virginica").
+iris("123", 7.7, 2.8, 6.7, 2, "virginica").
+iris("124", 6.3, 2.7, 4.9, 1.8, "virginica").
+iris("125", 6.7, 3.3, 5.7, 2.1, "virginica").
+iris("126", 7.2, 3.2, 6, 1.8, "virginica").
+iris("127", 6.2, 2.8, 4.8, 1.8, "virginica").
+iris("128", 6.1, 3, 4.9, 1.8, "virginica").
+iris("129", 6.4, 2.8, 5.6, 2.1, "virginica").
+iris("130", 7.2, 3,   5.8, 1.6, "virginica").
+iris("131", 7.4, 2.8, 6.1, 1.9, "virginica").
+iris("132", 7.9, 3.8, 6.4, 2, "virginica").
+iris("133", 6.4, 2.8, 5.6, 2.2, "virginica").
+iris("134", 6.3, 2.8, 5.1, 1.5, "virginica").
+iris("135", 6.1, 2.6, 5.6, 1.4, "virginica").
+iris("136", 7.7, 3, 6.1, 2.3, "virginica").
+iris("137", 6.3, 3.4, 5.6, 2.4, "virginica").
+iris("138", 6.4, 3.1, 5.5, 1.8, "virginica").
+iris("139", 6 3, 4.8, 1.8, 0.0, "virginica").
+iris("140", 6.9, 3.1, 5.4, 2.1, "virginica").
+iris("141", 6.7, 3.1, 5.6, 2.4, "virginica").
+iris("142", 6.9, 3.1, 5.1, 2.3, "virginica").
+iris("143", 5.8, 2.7, 5.1, 1.9, "virginica").
+iris("144", 6.8, 3.2, 5.9, 2.3, "virginica").
+iris("145", 6.7, 3.3, 5.7, 2.5, "virginica").
+iris("146", 6.7, 3,   5.2, 2.3, "virginica").
+iris("147", 6.3, 2.5, 5,   1.9, "virginica").
+iris("148", 6.5, 3, 5.2, 2, "virginica").
+iris("149", 6.2, 3.4, 5.4, 2.3, "virginica").
+iris("150", 5.9, 3,   5.1, 1.8, "virginica").
+
+
+
+%It is taken from http://kti.ms.mff.cuni.cz/~bartak/prolog/sorting.html
+%Example: insert_sort([1,10,3],S). S is sorted list.
+
+insert_sort(List,Sorted):-i_sort(List,[],Sorted).
+i_sort([],Acc,Acc).
+i_sort([H|T],Acc,Sorted):-insert(H,Acc,NAcc),i_sort(T,NAcc,Sorted).
+   
+insert(X,[Y|T],[Y|NT]):- X > Y,insert(X,T,NT).
+insert(X,[Y|T],[X,Y|T]):- X =< Y.
+insert(X,[],[X]).
+
+%It calculates euler distance between given point and other given point set.
+%Example: euler("1",["1","2"],[],Bcc,_). It returns -> Bcc = [0.0, 0.5385164807134502] .
+
+euler(_,[],S,S,_).
+euler(X,[H|T],Acc,Bcc,_) :- iris(X,X1,X2,X3,X4,_), iris(H,Y1,Y2,Y3,Y4,_),
+	D is sqrt(((X1-Y1) * (X1-Y1)) + ((X2-Y2) * (X2-Y2)) + ((X3-Y3) * (X3-Y3)) + ((X4-Y4) * (X4-Y4))),
+	append(Acc,[D],K), euler(X,T,K,Bcc,_).
+
+
+%It calculates all the distances between given point and other point set, at the end it creates a row for a given point
+%which store nearest distances for that point in ascending order.
+%Example: sorted("1",Sorted). It gives a row calculated distances with "1" -> Sorted = [0.0, 0.09999999999999998, 0.1414213562373093, 0.14142135623730964, 0.14142135623730995, 0.14142135623730995, 0.17320508075688743, 0.17320508075688762, 0.22360679774997896|...] .
+
+sorted(X,Sorted) :- helper([],R,"0"), euler(X,R,[],K,_), insert_sort(K,Sorted).
+
+
+
+%When it used with distanceMatrixCaller it calculates distance matrix for existing dataset.
+%Example: distanceMatrixCaller(U). It returns U such as in the format of [[a,a,a,a],[a,a,a,a],[a,a,a,a],[a,a,a,a]]
+
+distanceMatrix([],S,S).
+distanceMatrix([H|T],Acc,Bcc) :- helper([],R,"0"), euler(H,R,[],Mat,_), append(Acc,[Mat],K), distanceMatrix(T,K,Bcc).
+distanceMatrixCaller(Bcc) :- helper([],R,"0"), distanceMatrix(R,[],Bcc).
+
+
+
+%It finds kth element of given list.
+/*
+Example: kth(1,[0,1,4,6,7],K). -> K = 1.
+		  kth(0,[0,1,4,6,7],K). -> K = 0.
+		  kth(3,[0,1,4,6,7],K). -> K = 6 .
+*/
+
+kth(_,[],_,_).
+kth(0,[H|_],H).
+kth(X,[_|T],K) :- L is X - 1, kth(L,T,K).
+
+
+
+%For given point value it calculates kth nearest neighbour; distance(N), Location("?"), Type("?")
+%Example: 
+/*	
+	?- neighbour("149",2,N,Loc,Type).
+	N = 0.3000000000000001,
+	Loc = 116,
+	Type = "virginica" . 
+
+	For checking the value some possible options:
+		?- euler("149",["116"],[],Bcc,_).
+		Bcc = [0.3000000000000001] .
+
+		sorted("149",Sorted).
+		Sorted = [0.0, 0.2449489742783171, 0.3000000000000001, 0.5567764362830023, 0.6164414002968975, 0.6244997998398395, 0.6244997998398396, 0.6244997998398396, 0.6244997998398398|...]
+
+		It can be seen data third position is the value which we are expecting since we didnt count itself.
+*/
+
+neighbour(Point,K,N,Location,Type) :- sorted(Point,Sorted), kth(K,Sorted,N), helper([],R,"0"), euler(Point,R,[],Bcc,_), search(N,Bcc,1,Location), 
+	atom_string(Location,S2), iris(S2,_,_,_,_,Type).
+
+
+
+%It combine with neighbour predicate and returns Kth nearest elements from 0 to K value as a list.
+%Example:
+/*
+	?- neighbours("1",3,[],R).
+	R = [[3, 0.14142135623730964, 40, "setosa"], [2, 0.1414213562373093, 5, "setosa"], [1, 0.09999999999999998, 18, "setosa"]] .
+
+	First item in R, K value, second N(distance), third matched point value, fourth is its type for matched value.
+
+	?- neighbours("148",4,[],R).
+	R = [[4, 0.3605551275463989, 146, "virginica"], [3, 0.3605551275463988, 117, "virginica"], [2, 0.34641016151377513, 112, "virginica"], [1, 0.22360679774997935, 111, "virginica"]] 
+
+*/
+
+neighbours(_,0,L,L).
+neighbours(Point,K,L,R) :- neighbour(Point,K,N,Loc,Type), append(L,[[K,N,Loc,Type]],P), O is K - 1, neighbours(Point,O,P,R).
+
+
+%It is basic search implementation it searches for given item in the given list.
+%Example: 
+/*
+	?- search(1,[2,1,5,3],1,R). (third argument is offset value)
+	R = 2 .
+
+	?- search(3,[1,2,5,3],1,R).
+	R = 4 .
+
+*/
+
+search(X,[X|_],K,K).
+search(X,[_|T],K,R) :- L is K + 1, search(X,T,L,R). 
+
+
+
+%It creates a list consist of given data point values.
+%Example:
+/*
+	?- helper([],R,"0").
+	R = ["1", "2", "3", "4", "5", "6", "7", "8", "9"|...] .
+*/ 
+
+helper(Acc,R,A) :- iris(X,_,_,_,_,_), atom_number(A,B), atom_number(X,C), C > B, append(Acc,[X],L), helper(L,R,X).
+helper(Acc,Acc,_).
+
+
+
+
+
+
